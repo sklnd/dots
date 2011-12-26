f998990ae461bbb07cacaecbfd38b8b75992144b
Chris Skalenda <chris@skalenda.org>
2011-12-26 16:45:07 -0700

    pep8, more comments.

--- deploydots.py
+++ deploydots.py
@@ -12,6 +12,7 @@ import shutil
 # Blacklisted files
 blacklist = [os.path.basename(__file__), 'backup']
 
+
 # Stolen from a stackoverflow post on how to print term colors, came originally
 # from blender build scripts.
 class bcolors:
@@ -22,42 +23,49 @@ class bcolors:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
+
 def cprint(string, color):
     print color + string + bcolors.ENDC
 
+
 def sievedot(dot):
-  if dot[0] == '.' or dot in blacklist:
-    return False
-  else:
-    return True
+    if dot[0] == '.' or dot in blacklist:
+        return False
+    else:
+        return True
+
 
 def linkdot(dot):
-  cwd = os.getcwd()
-  homepath = os.getenv("HOME")
-  linkpath = homepath + os.sep + '.' + dot
-  dotpath = cwd + os.sep + dot
+    cwd = os.getcwd()
+    homepath = os.getenv("HOME")
+    linkpath = homepath + os.sep + '.' + dot
+    dotpath = cwd + os.sep + dot
 
-  if os.path.exists(linkpath):
-    # If the file is a symlink pointing to the dots directory, it is previously
-    # deployed from this repo. Print a log line and ignore it.
-    if os.path.realpath(linkpath) == dotpath:
-      cprint("Previously deployed file:\t\t%s" % (linkpath), bcolors.OKBLUE)
-    # Otherwise, back up the existing file
+    if os.path.exists(linkpath):
+        # If the file is a symlink pointing to the dots directory, it is
+        # previously deployed from this repo. Print a log line and ignore it.
+        if os.path.realpath(linkpath) == dotpath:
+            cprint("Previously deployed file:\t\t%s" % (linkpath),
+                    bcolors.OKBLUE)
+         # Otherwise, back up the existing file
+        else:
+            backupdir = cwd + os.sep + "backup"
+            if not os.path.exists(backupdir):
+                os.mkdir(backupdir)
+
+            # Backup the file.
+            cprint("Backing up file:\t\t\t%s" % (linkpath), bcolors.WARNING)
+            if os.path.exists(backupdir + os.sep + dot):
+                os.remove(backupdir + os.sep + dot)
+            shutil.move(linkpath, backupdir + os.sep + dot)
+
+            # Go ahead and deploy the file.
+            cprint("Deploying %s:\t\t\t%s" % (dot, linkpath), bcolors.OKGREEN)
+            os.symlink(dotpath, linkpath)
+
     else:
-      backupdir = cwd + os.sep + "backup"
-      if not os.path.exists(backupdir):
-        os.mkdir(backupdir)
-
-      cprint("Backing up file:\t\t\t%s" % (linkpath), bcolors.WARNING)
-      if os.path.exists(backupdir + os.sep + dot):
-        os.remove(backupdir + os.sep + dot)
-      shutil.move(linkpath, backupdir + os.sep + dot)
-
-      cprint("Deploying %s:\t\t\t%s" % (dot, linkpath), bcolors.OKGREEN)
-      os.symlink(dotpath, linkpath)
-  else:
-    cprint("Deploying %s:\t\t\t%s" % (dot, linkpath), bcolors.OKGREEN)
-    os.symlink(dotpath, linkpath)
+        cprint("Deploying %s:\t\t\t%s" % (dot, linkpath), bcolors.OKGREEN)
+        os.symlink(dotpath, linkpath)
 
 
 dots = os.listdir('.')
